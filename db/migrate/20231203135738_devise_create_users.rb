5ac801413ecf985b9a77bba2cec01e6cea9ddb27
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-12-03 23:16:56 +0900

    font-awesome

--- db/migrate/20231203135738_devise_create_users.rb
+++ db/migrate/20231203135738_devise_create_users.rb
@@ -6,6 +6,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :username
+      t.string :profile_image_id
 
       ## Recoverable
       t.string   :reset_password_token
