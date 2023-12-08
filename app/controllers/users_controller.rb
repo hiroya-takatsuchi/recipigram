49aa26b189eed31fbdf6d71cf49c97385719237e
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-12-08 23:49:45 +0900

    更新機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,17 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+  @user = User.find(params[:id])
+  if @user.update(user_params)
+    redirect_to user_path(@user), notice: "ユーザー情報を更新しました。"
+  else
+    render :edit
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :profile, :profile_image)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
 
 end
