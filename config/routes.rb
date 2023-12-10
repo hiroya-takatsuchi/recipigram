b9de5bf29ca8a0055095650eb6e8435678d38aa3
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-12-10 13:56:50 +0900

    レシピ関連の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :users, only: [:index, :show, :edit, :update]
+  resources :recipes
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_03_135738) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_10_044951) do
+  create_table "recipes", charset: "utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.string "image_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
